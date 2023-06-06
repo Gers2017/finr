563bb10f61faf7feb5d1d197a8fea281aee267d6
Gers2017 <Gers2017@outlook.com>
2023-06-06 14:39:53 -0600

    refactor: Better error handling
    - update print_help
    - remove expect from command line parsing

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,18 @@ use std::path::{Path, PathBuf};
 
 fn print_help() {
     println!("finr [PATTERN] [PATH?] [FLAGS...]");
-    println!("");
-    println!("{:<20} {}", "--max-depth | -d", "Max recursion depth");
-    println!("{:<20} {}", "--type | -t", "Type of entry to search");
-    println!("{:<20} {}", "--ignore | -i", "Directories to ignore");
+    println!();
+    println!("{:<20} Max recursion depth", "--max-depth | -d");
+    println!("{:<20} Type of entry to search", "--type | -t");
+    println!("{:<20} Directories to ignore", "--ignore | -i");
     println!(
-        "{:<20} {}",
-        "--extension | -e", "Use [PATTERN] to search at the end"
+        "{:<20} Use [PATTERN] to search at the end",
+        "--extension | -e"
     );
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut args_iter = env::args().skip(1).into_iter().peekable();
+    let mut args_iter = env::args().skip(1).peekable();
     let mut config = Config::default();
 
     if let Some(target) = args_iter.next() {
@@ -43,20 +43,24 @@ fn main() -> anyhow::Result<()> {
         if arg == "--max-depth" || arg == "-d" {
             config.max_depth = args_iter
                 .next()
-                .expect("Missing Argument for --max-depth flag")
-                .parse()
-                .expect("A number");
+                .ok_or(anyhow::anyhow!("Missing argument for --max-depth flag"))?
+                .parse::<usize>()
+                .context("Argument for --max-depth should be a positive integer")?;
         }
 
         if arg == "--type" || arg == "-t" {
-            config.entry_type = args_iter
+            let arg = args_iter
                 .next()
-                .map(|arg| match arg.as_str() {
-                    "file" | "f" => EntryType::File,
-                    "directory" | "d" => EntryType::Directory,
-                    _ => panic!("Invalid arg for --type flag!"),
-                })
-                .expect("Missing Argument for --type flag");
+                .ok_or(anyhow::anyhow!("Missing Argument for --type flag"))?;
+
+            config.entry_type = match arg.as_str() {
+                "file" | "f" => Ok(EntryType::File),
+                "directory" | "d" => Ok(EntryType::Directory),
+                _ => anyhow::bail!(
+                    "Invalid argument \"{}\" for --type flag. Valid arguments [file | directory]",
+                    arg
+                ),
+            }?;
         }
 
         if arg == "--ignore" || arg == "-i" {
@@ -67,9 +71,13 @@ fn main() -> anyhow::Result<()> {
 
                 let dir = args_iter
                     .next()
-                    .expect("Missing Argument for --ignore flag")
+                    .ok_or(anyhow::anyhow!("Missing argument for --ignore flag"))?
                     .trim()
                     .to_string();
+
+                if dir.is_empty() {
+                    anyhow::bail!("Invalid empty directory \"{}\"", dir);
+                }
 
                 config.ignore_dirs.push(dir);
             }
@@ -184,10 +192,8 @@ pub fn find<P: AsRef<Path>>(
         let name = entry.file_name().to_string_lossy().to_string();
         let is_dir = entry.file_type().context("File type Error")?.is_dir();
 
-        if config.entry_type.is_dir() == is_dir {
-            if config.is_match(&name) {
-                result.entries.push(entry.path().display().to_string());
-            }
+        if config.entry_type.is_dir() == is_dir && config.is_match(&name) {
+            result.entries.push(entry.path().display().to_string());
         }
 
         if is_dir {
