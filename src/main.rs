1373c182294e48fb7f0e6ae51f64265360f78481
Gers2017 <Gers2017@outlook.com>
2023-06-07 09:43:27 -0600

    refactor: Move structs and functions to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
 use anyhow::{Context, Ok};
-use std::collections::HashSet;
+use finr::{find, print_help, Config, EntryType};
 use std::env;
-use std::fs::read_dir;
-use std::path::{Path, PathBuf};
-
-fn print_help() {
-    println!("finr [PATTERN] [PATH?] [FLAGS...]");
-    println!();
-    println!("{:<20} Max recursion depth", "--max-depth | -d");
-    println!("{:<20} Type of entry to search", "--type | -t");
-    println!("{:<20} Directories to ignore", "--ignore | -i");
-    println!("{:<20} Directories to include", "--include | -n");
-    println!(
-        "{:<20} Use [PATTERN] to search at the end",
-        "--extension | -e"
-    );
-}
+use std::path::PathBuf;
 
 fn main() -> anyhow::Result<()> {
     let mut args_iter = env::args().skip(1).peekable();
@@ -112,103 +98,6 @@ fn main() -> anyhow::Result<()> {
 
     for e in result.iter() {
         println!("{}", e.display());
-    }
-
-    Ok(())
-}
-
-#[derive(Debug)]
-pub struct Config {
-    pub target: String,
-    pub entry_type: EntryType,
-    pub is_extension: bool,
-    pub max_depth: usize,
-    pub ignore_dirs: HashSet<String>,
-    pub include_dirs: HashSet<String>,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        let ignore_dirs: HashSet<_> = [
-            "node_modules",
-            "target",
-            ".git",
-            ".cargo",
-            ".rustup",
-            ".npm",
-            ".ssh",
-            "__pycache__",
-        ]
-        .into_iter()
-        .map(|item| item.to_string())
-        .collect();
-
-        Self {
-            target: String::new(),
-            entry_type: EntryType::File,
-            is_extension: false,
-            max_depth: 100,
-            ignore_dirs,
-            include_dirs: HashSet::default(),
-        }
-    }
-}
-
-impl Config {
-    fn is_match(&self, filename: &str) -> bool {
-        if self.is_extension {
-            filename.ends_with(&self.target)
-        } else {
-            filename.contains(&self.target)
-        }
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-pub enum EntryType {
-    File = 0,
-    Directory = 1,
-}
-
-impl EntryType {
-    pub fn is_dir(&self) -> bool {
-        use EntryType::*;
-        match self {
-            File => false,
-            Directory => true,
-        }
-    }
-}
-
-pub fn find<P: AsRef<Path>>(
-    root: P,
-    depth: usize,
-    config: &Config,
-    result: &mut Vec<PathBuf>,
-) -> anyhow::Result<()> {
-    if depth > config.max_depth {
-        return Ok(());
-    }
-
-    let entries = read_dir(root)?;
-
-    for entry in entries.into_iter().filter_map(Result::ok) {
-        let name = entry.file_name().to_string_lossy().to_string();
-        let is_dir = entry.file_type().context("File type Error")?.is_dir();
-
-        if config.entry_type.is_dir() == is_dir && config.is_match(&name) {
-            result.push(entry.path());
-        }
-
-        if is_dir {
-            if !config.include_dirs.contains(&name)
-                && (name.starts_with('.') || config.ignore_dirs.contains(&name))
-            {
-                continue;
-            }
-
-            find(entry.path(), depth + 1, config, result)?;
-        }
     }
 
     Ok(())
