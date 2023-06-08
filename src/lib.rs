526a5b527bd224bb39fce74fc79de4221d3b079f
Vaimer9 <vaim3r9@gmail.com>
2023-06-08 15:11:08 +0530

    Change CLI to use clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,39 @@
 use anyhow::{Context, Ok};
-use std::collections::HashSet;
 use std::fs::read_dir;
 use std::path::{Path, PathBuf};
 
-#[derive(Debug)]
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
 pub struct Config {
     pub target: String,
-    pub entry_type: EntryType,
-    pub is_extension: bool,
+
+    #[arg(short = 'r')]
+    pub dir: bool,
+
+    #[arg(short = 'e')]
+    pub extension: bool,
+
+    #[arg(short = 'd', default_value = "100")]
     pub max_depth: usize,
-    pub ignore_dirs: HashSet<String>,
-    pub include_dirs: HashSet<String>,
-}
 
-impl Default for Config {
-    fn default() -> Self {
-        let ignore_dirs: HashSet<_> = [
-            "node_modules",
-            "target",
-            ".git",
-            ".cargo",
-            ".rustup",
-            ".npm",
-            ".ssh",
-            "__pycache__",
-        ]
-        .into_iter()
-        .map(|item| item.to_string())
-        .collect();
+    #[arg(short = 'I')]
+    pub ignore_dirs: Vec<String>,
 
-        Self {
-            target: String::new(),
-            entry_type: EntryType::File,
-            is_extension: false,
-            max_depth: 100,
-            ignore_dirs,
-            include_dirs: HashSet::default(),
-        }
-    }
+    #[arg(short = 'i')]
+    pub include_dirs: Vec<String>,
+
+    #[arg(short = 'p')]
+    pub path: Option<PathBuf>
 }
 
 impl Config {
     pub fn is_match(&self, filename: &str) -> bool {
-        if self.is_extension {
+        if self.extension {
             filename.ends_with(&self.target)
         } else {
             filename.contains(&self.target)
-        }
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-pub enum EntryType {
-    File = 0,
-    Directory = 1,
-}
-
-impl EntryType {
-    pub fn is_dir(&self) -> bool {
-        use EntryType::*;
-        match self {
-            File => false,
-            Directory => true,
         }
     }
 }
@@ -80,9 +52,9 @@ pub fn find<P: AsRef<Path>>(
 
     for entry in entries.into_iter().filter_map(Result::ok) {
         let name = entry.file_name().to_string_lossy().to_string();
-        let is_dir = entry.file_type().context("File type Error")?.is_dir();
+        let is_dir = entry.file_type().context("File Type Error")?.is_dir();
 
-        if config.entry_type.is_dir() == is_dir && config.is_match(&name) {
+        if config.dir == is_dir && config.is_match(&name) {
             result.push(entry.path());
         }
 
@@ -98,29 +70,4 @@ pub fn find<P: AsRef<Path>>(
     }
 
     Ok(())
-}
-
-pub fn print_help() {
-    println!("finr [PATTERN] [PATH?] [FLAGS...]");
-    println!();
-    println!(
-        "{:<20} Max recursion depth. By default is 100",
-        "--max-depth | -d"
-    );
-    println!(
-        "{:<20} Type of entry to search. Possible values: f | file | directory | d",
-        "--type | -t"
-    );
-    println!(
-        "{:<20} Directories to ignore. Expects a name not a path",
-        "--ignore | -i"
-    );
-    println!(
-        "{:<20} Directories to include. Expects a name not a path",
-        "--include | -n"
-    );
-    println!(
-        "{:<20} Use [PATTERN] to match at the end of the file or directory",
-        "--extension | -e"
-    );
 }
