2e5381a9e1e6d9cca02b4de05df29f6fd6411f56
Gers2017 <Gers2017@outlook.com>
2023-06-08 16:37:37 -0600

    feat: Add bpaf for command line argument parsing

--- src/lib.rs
+++ src/lib.rs
@@ -3,46 +3,20 @@ use std::collections::HashSet;
 use std::fs::read_dir;
 use std::path::{Path, PathBuf};
 
-#[derive(Debug)]
-pub struct Config {
+#[derive(Debug, Clone)]
+pub struct Out {
     pub target: String,
-    pub entry_type: EntryType,
-    pub is_extension: bool,
+    pub path: PathBuf,
     pub max_depth: usize,
-    pub ignore_dirs: HashSet<String>,
-    pub include_dirs: HashSet<String>,
+    pub is_dir: bool,
+    pub ignore: HashSet<String>,
+    pub include: HashSet<String>,
+    pub extension: bool,
 }
 
-impl Default for Config {
-    fn default() -> Self {
-        let ignore_dirs: HashSet<_> = [
-            "node_modules",
-            "target",
-            ".git",
-            ".cargo",
-            ".rustup",
-            ".npm",
-            ".ssh",
-            "__pycache__",
-        ]
-        .into_iter()
-        .map(|item| item.to_string())
-        .collect();
-
-        Self {
-            target: String::new(),
-            entry_type: EntryType::File,
-            is_extension: false,
-            max_depth: 100,
-            ignore_dirs,
-            include_dirs: HashSet::default(),
-        }
-    }
-}
-
-impl Config {
+impl Out {
     pub fn is_match(&self, filename: &str) -> bool {
-        if self.is_extension {
+        if self.extension {
             filename.ends_with(&self.target)
         } else {
             filename.contains(&self.target)
@@ -50,26 +24,10 @@ impl Config {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
-pub enum EntryType {
-    File = 0,
-    Directory = 1,
-}
-
-impl EntryType {
-    pub fn is_dir(&self) -> bool {
-        use EntryType::*;
-        match self {
-            File => false,
-            Directory => true,
-        }
-    }
-}
-
 pub fn find<P: AsRef<Path>>(
     root: P,
     depth: usize,
-    config: &Config,
+    config: &Out,
     result: &mut Vec<PathBuf>,
 ) -> anyhow::Result<()> {
     if depth > config.max_depth {
@@ -82,13 +40,13 @@ pub fn find<P: AsRef<Path>>(
         let name = entry.file_name().to_string_lossy().to_string();
         let is_dir = entry.file_type().context("File type Error")?.is_dir();
 
-        if config.entry_type.is_dir() == is_dir && config.is_match(&name) {
+        if config.is_dir == is_dir && config.is_match(&name) {
             result.push(entry.path());
         }
 
         if is_dir {
-            if !config.include_dirs.contains(&name)
-                && (name.starts_with('.') || config.ignore_dirs.contains(&name))
+            if !config.include.contains(&name)
+                && (name.starts_with('.') || config.ignore.contains(&name))
             {
                 continue;
             }
@@ -98,29 +56,4 @@ pub fn find<P: AsRef<Path>>(
     }
 
     Ok(())
-}
-
-pub fn print_help() {
-    println!("finr [PATTERN] [PATH?] [FLAGS...]");
-    println!();
-    println!(
-        "{:<20} Max recursion depth. By default is 100",
-        "--max-depth | -d"
-    );
-    println!(
-        "{:<20} Type of entry to search. Possible values: f | file | directory | d",
-        "--type | -t"
-    );
-    println!(
-        "{:<20} Directories to ignore. Expects a name not a path",
-        "--ignore | -i"
-    );
-    println!(
-        "{:<20} Directories to include. Expects a name not a path",
-        "--include | -n"
-    );
-    println!(
-        "{:<20} Use [PATTERN] to match at the end of the file or directory",
-        "--extension | -e"
-    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,74 @@
-use anyhow::{Context, Ok};
-use finr::{find, print_help, Config, EntryType};
+use anyhow::Ok;
+use bpaf::*;
+use finr::{find, Out};
+use std::collections::HashSet;
 use std::env;
-use std::path::PathBuf;
+
+fn opts() -> anyhow::Result<OptionParser<Out>> {
+    let target = positional::<String>("TARGET").help("Name to use in search");
+    let path = positional("PATH")
+        .help("Path to start search")
+        .fallback(env::current_dir()?);
+    let max_depth = short('m')
+        .long("max-depth")
+        .help("Max recursion depth. By default is 100")
+        .argument::<usize>("M")
+        .fallback(100);
+    let is_dir = short('d')
+        .long("dir")
+        .help("Search for directories or files")
+        .switch();
+    let ignore = short('i')
+        .long("ignore")
+        .help("Directories to ignore. Expects a name not a path")
+        .argument::<String>("IGNORE")
+        .many()
+        .parse(|x| {
+            let mut h: HashSet<String> = [
+                "node_modules",
+                "target",
+                ".git",
+                ".cargo",
+                ".rustup",
+                ".npm",
+                ".ssh",
+                "__pycache__",
+            ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+            h.extend(x.into_iter());
+            Ok(h)
+        });
+
+    let include = short('n')
+        .long("include")
+        .help("Directories to include. Expects a name not a path")
+        .argument::<String>("INCLUDE")
+        .many()
+        .parse(|x| Ok(HashSet::from_iter(x.to_owned().into_iter())));
+    let extension = short('e')
+        .long("extension")
+        .help("match at the end of the file or directory")
+        .switch();
+
+    Ok(construct!(Out {
+        max_depth,
+        is_dir,
+        ignore,
+        include,
+        extension,
+        target,
+        path,
+    })
+    .to_options())
+}
 
 fn main() -> anyhow::Result<()> {
-    let mut args_iter = env::args().skip(1).peekable();
-    let mut config = Config::default();
-
-    if let Some(target) = args_iter.next() {
-        if target == "--help" {
-            print_help();
-            return Ok(());
-        }
-
-        config.target = target;
-    } else {
-        print_help();
-        return Ok(());
-    }
-
-    let mut path = env::current_dir()?;
-
-    if let Some(arg) = args_iter.peek() {
-        if !arg.starts_with('-') {
-            path = PathBuf::from(arg);
-        }
-    }
-
-    while let Some(arg) = args_iter.next() {
-        if arg == "--max-depth" || arg == "-d" {
-            config.max_depth = args_iter
-                .next()
-                .ok_or(anyhow::anyhow!("Missing argument for --max-depth flag"))?
-                .parse::<usize>()
-                .context("Argument for --max-depth should be a positive integer")?;
-        }
-
-        if arg == "--type" || arg == "-t" {
-            let arg = args_iter
-                .next()
-                .ok_or(anyhow::anyhow!("Missing Argument for --type flag"))?;
-
-            config.entry_type = match arg.as_str() {
-                "file" | "f" => Ok(EntryType::File),
-                "directory" | "d" => Ok(EntryType::Directory),
-                _ => anyhow::bail!(
-                    "Invalid argument \"{}\" for --type flag. Valid arguments [file | directory]",
-                    arg
-                ),
-            }?;
-        }
-
-        if arg == "--ignore" || arg == "-i" {
-            while let Some(current) = args_iter.peek() {
-                if current.starts_with('-') {
-                    break;
-                }
-
-                let dir = args_iter.next().unwrap().trim().to_string();
-
-                if dir.is_empty() {
-                    anyhow::bail!("Invalid value for --ignore argument: \"{}\"", dir);
-                }
-
-                config.ignore_dirs.insert(dir);
-            }
-        }
-
-        if arg == "--include" || arg == "-n" {
-            while let Some(current) = args_iter.peek() {
-                if current.starts_with('-') {
-                    break;
-                }
-
-                let dir = args_iter.next().unwrap().trim().to_string();
-
-                if dir.is_empty() {
-                    anyhow::bail!("Invalid value for --include argument: \"{}\"", dir);
-                }
-
-                config.include_dirs.insert(dir);
-            }
-        }
-
-        if arg == "--extension" || arg == "-e" {
-            config.is_extension = true
-        }
-
-        if arg == "--help" {
-            print_help();
-            return Ok(());
-        }
-    }
+    let cli = opts()?.run();
 
     let mut result = Vec::with_capacity(6);
-    find(path, 0, &config, &mut result)?;
+    find(&cli.path, 0, &cli, &mut result)?;
 
     for e in result.iter() {
         println!("{}", e.display());
