fdd72a1c4d5bff065dd9575d70ac07bbd0094489
Gers2017 <Gers2017@outlook.com>
2023-06-09 20:24:50 -0600

    feat: Add cli tests
    - add regex search test
    - add invalid target test

---
+++ tests/cli.rs
@@ -0,0 +1,46 @@
+use anyhow::Ok;
+use assert_cmd::prelude::*;
+use assert_fs::{
+    self,
+    prelude::{FileTouch, PathChild},
+};
+use predicates::{prelude::*, str::contains};
+use std::process::Command;
+
+#[test]
+fn search_files_regex() -> anyhow::Result<()> {
+    let root = assert_fs::TempDir::new()?;
+
+    let paths = [
+        "user/code/projects/secrets/finr2/src/main.rs",
+        "user/code/avocado/src/lib.rs",
+        "user/code/gopkg/src/main.go",
+        "user/code/gopkg/src/utils/mod.rs",
+    ];
+
+    for path in paths {
+        root.child(path).touch()?;
+    }
+
+    let mut cmd = Command::cargo_bin("finr")?;
+    cmd.arg(".+\\.rs").arg(root.path()).arg("-R");
+
+    cmd.assert().success().stdout(
+        contains("main.rs")
+            .and(contains("lib.rs"))
+            .and(contains("mod.rs")),
+    );
+
+    root.close()?;
+    Ok(())
+}
+
+#[test]
+fn invalid_target() -> anyhow::Result<()> {
+    let mut cmd = Command::cargo_bin("finr")?;
+    cmd.arg("--regex");
+
+    cmd.assert().failure().stderr(contains("Expected pattern"));
+
+    Ok(())
+}
