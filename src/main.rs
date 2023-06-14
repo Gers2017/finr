8c752b6546d806f103368163011e403096bb05ae
Gers2017 <Gers2017@outlook.com>
2023-06-14 12:48:29 -0600

    refactor(Print): lock stdout manually

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use anyhow::Ok;
 use finr::args::ParseResult;
 use finr::{args, find};
+use std::io::Write;
 
 fn main() -> anyhow::Result<()> {
     let ParseResult { config, path } = args::parse()?;
     let mut result = Vec::with_capacity(6);
 
+    // https://nnethercote.github.io/perf-book/io.html
+    let stdout = std::io::stdout();
+    let mut lock = stdout.lock();
+
     find(path, 0, &config, &mut result)?;
 
     for e in result.iter() {
-        println!("{}", e.display());
+        writeln!(lock, "{}", e.display())?;
     }
 
     Ok(())
